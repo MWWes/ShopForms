4f07120a2b7fc4aebce2c9dc0b322268209f0f08
Mww Wesley <Mww Wesley>
2017-04-02 18:58:58 -0400

    First commit

---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html'
+})
+export class ShoppingListPage {
+}
