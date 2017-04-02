f89f0424a4256aa674fe3b3da24c41e77f4ec1a3
Mww Wesley <Mww Wesley>
2017-04-02 19:18:15 -0400

    Subit The Form

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
 export class ShoppingListPage {
+  onAddItem(form: NgForm){
+    console.log(form);
+  }
 }
