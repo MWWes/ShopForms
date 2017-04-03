42477f332df3a4789e7d3a4b08c66eab102bbe8f
Mww Wesley <Mww Wesley>
2017-04-03 12:45:30 -0400

    Adding and Displaying Items

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 import { NgForm } from "@angular/forms";
+import { ShoppingListService } from "../../services/shopping-list";
+import { Ingredient } from "../../models/ingredient";
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
+  constructor(private slService: ShoppingListService){}
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
   onAddItem(form: NgForm){
-    console.log(form);
+    this.slService.addItem(form.value.ingredientName, form.value.amount);
+    form.reset();
+    this.loadItems();
   }
 }
